58d4667ef8ba5a9a095946b5972846650bedd997
aliqasimkhundmiri10@gmail.com <rehaan@Guest>
2024-03-22 23:43:59 -0400

    Assignment 3

---
+++ assignment/Assignment - 3/WebApplication1/WebApplication1/Controllers/TeacherDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+
+
+namespace WebApplication1.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+      [HttpGet]
+      [Route("api/teacherdata/listteachers")]
+
+        public string ListTeachers()
+        {
+            return "good morning";
+        }
+    }
+}
