d011d78fabba451a9e10c06686b2741eeb407c65
aliqasimkhundmiri10@gmail.com <rehaan@Guest>
2024-03-23 15:22:10 -0400

    updated

---
+++ assignment/Assignment - 3/WebApplication1/WebApplication1/Controllers/ClassesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClassesController : Controller
+    {
+        // GET: Classes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            ClassesDataController controller = new ClassesDataController();
+            IEnumerable<Classes> Classes = controller.ListClasses();
+            return View(Classes);
+        }
+
+        //GET : /Author/Show/{id}
+        public ActionResult Show(int id)
+        {
+            ClassesDataController controller = new ClassesDataController();
+            Classes NewClass = controller.FindClasses(id);
+
+
+            return View(NewClass);
+        }
+}
---
+++ assignment/Assignment - 3/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET : /Student/List
+        public ActionResult List()
+        {
+            StudentDataController controller = new StudentDataController();
+            IEnumerable<Student> Students = controller.ListStudents();
+            return View(Students);
+        }
+
+        //GET : /Student/Show/{id}
+        public ActionResult Show(int id)
+        {
+            StudentDataController controller = new StudentDataController();
+            Student NewStudent = controller.FindStudent(id);
+
+
+            return View(NewStudent);
+        }
+    }
+}
